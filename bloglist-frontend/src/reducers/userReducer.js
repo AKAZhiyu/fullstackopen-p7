7ebfc19073f1b000fb77aaa015cd127684bbff99
AKAZhiyu <zhixuan.z2003@gmail.com>
2025-03-12 09:35:35 +0800

    finish all exercise in p7

--- bloglist-frontend/src/reducers/userReducer.js
+++ bloglist-frontend/src/reducers/userReducer.js
@@ -1,6 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit'
-import loginService from '../services/login'
-import blogService from '../services/blogs'
 
 const userSlice = createSlice({
   name: 'user',
